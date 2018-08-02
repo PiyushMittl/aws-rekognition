fca071792ccbe2b26185245ee073aa31e1864c6e
Piyush Mittal <mr.piyushmittal@rediffmail.com>
2018-08-03 00:34:22 +0530

    Update Constant.java

--- src/main/java/com/piyush/constant/Constant.java
+++ src/main/java/com/piyush/constant/Constant.java
@@ -3,7 +3,7 @@ package com.piyush.constant;
 public class Constant {
 
 	//security constatnt
-	public static String accessKey="AKIAJ22O2SM7AHRBYUYQ";
-	public static String secretKey="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";
+	public static String accessKey="";
+	public static String secretKey="";
 	
 }
